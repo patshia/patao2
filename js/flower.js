84e9eff1fef91661a1a2a08d2957655f2cdbaa03
patshia <111842731+patshia@users.noreply.github.com>
2023-05-26 13:27:24 -0700

    First Commit

---
+++ js/flower.js
@@ -0,0 +1,18 @@
+
+console.log("HELLO")
+const panels = document.querySelectorAll('.panel')
+
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        closePanel()
+        panel.classList.add('active')
+    })
+})
+
+
+function closePanel() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
